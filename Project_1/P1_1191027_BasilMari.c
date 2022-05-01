835c8cab9e75430832d449cad21c8abf511d0b6d
Basil Mari <basilm4r0@gmail.com>
2022-05-02 01:21:59 +0300

    add makefile, rename busses.txt, and debug program

--- Project_1/P1_1191027_BasilMari.c
+++ Project_1/P1_1191027_BasilMari.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <bsd/string.h>
 #pragma clang diagnostic ignored "-Wformat-security"
 
 //define passenger linked list node data structure
@@ -60,11 +59,11 @@ void InsertPassenger(char pid[20], char date[20], char time[20], char origin[20]
 		return;
 	}
 
-	strlcpy(temp->pid, pid, 20);
-	strlcpy(temp->date, date, 20);
-	strlcpy(temp->time, time, 20);
-	strlcpy(temp->origin, origin, 20);
-	strlcpy(temp->destination, destination, 20);
+	strncpy(temp->pid, pid, 20);
+	strncpy(temp->date, date, 20);
+	strncpy(temp->time, time, 20);
+	strncpy(temp->origin, origin, 20);
+	strncpy(temp->destination, destination, 20);
 	temp->next = P->next;
 	P->next = temp;
 }
@@ -93,23 +92,23 @@ void SwapPassengers(struct Passenger* L, struct Passenger* P) {
 	struct Passenger* T = MakeEmptyP(NULL);
 	if (T == NULL)
 		return;
-	strlcpy(T->pid, L->pid, 20);
-	strlcpy(T->date, L->date, 20);
-	strlcpy(T->time, L->time, 20);
-	strlcpy(T->origin, L->origin, 20);
-	strlcpy(T->destination, L->destination, 20);
+	strncpy(T->pid, L->pid, 20);
+	strncpy(T->date, L->date, 20);
+	strncpy(T->time, L->time, 20);
+	strncpy(T->origin, L->origin, 20);
+	strncpy(T->destination, L->destination, 20);
 
-	strlcpy(L->pid, P->pid, 20);
-	strlcpy(L->date, P->date, 20);
-	strlcpy(L->time, P->time, 20);
-	strlcpy(L->origin, P->origin, 20);
-	strlcpy(L->destination, P->destination, 20);
+	strncpy(L->pid, P->pid, 20);
+	strncpy(L->date, P->date, 20);
+	strncpy(L->time, P->time, 20);
+	strncpy(L->origin, P->origin, 20);
+	strncpy(L->destination, P->destination, 20);
 
-	strlcpy(P->pid, T->pid, 20);
-	strlcpy(P->date, T->date, 20);
-	strlcpy(P->time, T->time, 20);
-	strlcpy(P->origin, T->origin, 20);
-	strlcpy(P->destination, T->destination, 20);
+	strncpy(P->pid, T->pid, 20);
+	strncpy(P->date, T->date, 20);
+	strncpy(P->time, T->time, 20);
+	strncpy(P->origin, T->origin, 20);
+	strncpy(P->destination, T->destination, 20);
 
 	free(T);
 }
@@ -181,11 +180,11 @@ void InsertBus(char bid[20], char date[20], char time[20], char origin[20], char
 		return;
 	}
 
-	strlcpy(temp->bid, bid, 20);
-	strlcpy(temp->date, date, 20);
-	strlcpy(temp->time, time, 20);
-	strlcpy(temp->origin, origin, 20);
-	strlcpy(temp->destination, destination, 20);
+	strncpy(temp->bid, bid, 20);
+	strncpy(temp->date, date, 20);
+	strncpy(temp->time, time, 20);
+	strncpy(temp->origin, origin, 20);
+	strncpy(temp->destination, destination, 20);
 	temp->ticketprice = ticketprice;
 	temp->capacity = capacity;
 	temp->next = P->next;
@@ -193,7 +192,7 @@ void InsertBus(char bid[20], char date[20], char time[20], char origin[20], char
 
 }
 
-void PrintBusses(struct Bus* L) {
+void PrintBuses(struct Bus* L) {
 	struct Bus* P;
 	P = L;
 	if (IsEmptyB(L))
@@ -227,28 +226,28 @@ void SwapBuses(struct Bus* L, struct Bus* P) {
 	struct Bus* T = MakeEmptyB(NULL);
 	if (T == NULL)
 		return;
-	strlcpy(T->bid, L->bid, 20);
-	strlcpy(T->date, L->date, 20);
-	strlcpy(T->time, L->time, 20);
-	strlcpy(T->origin, L->origin, 20);
-	strlcpy(T->destination, L->destination, 20);
+	strncpy(T->bid, L->bid, 20);
+	strncpy(T->date, L->date, 20);
+	strncpy(T->time, L->time, 20);
+	strncpy(T->origin, L->origin, 20);
+	strncpy(T->destination, L->destination, 20);
 	T->ticketprice = L->ticketprice;
 	T->capacity = L->capacity;
 
-	strlcpy(L->bid, P->bid, 20);
-	strlcpy(L->date, P->date, 20);
-	strlcpy(L->time, P->time, 20);
-	strlcpy(L->origin, P->origin, 20);
-	strlcpy(L->destination, P->destination, 20);
+	strncpy(L->bid, P->bid, 20);
+	strncpy(L->date, P->date, 20);
+	strncpy(L->time, P->time, 20);
+	strncpy(L->origin, P->origin, 20);
+	strncpy(L->destination, P->destination, 20);
 	L->ticketprice = P->ticketprice;
 	L->capacity = P->capacity;
 
 
-	strlcpy(P->bid, T->bid, 20);
-	strlcpy(P->date, T->date, 20);
-	strlcpy(P->time, T->time, 20);
-	strlcpy(P->origin, T->origin, 20);
-	strlcpy(P->destination, T->destination, 20);
+	strncpy(P->bid, T->bid, 20);
+	strncpy(P->date, T->date, 20);
+	strncpy(P->time, T->time, 20);
+	strncpy(P->origin, T->origin, 20);
+	strncpy(P->destination, T->destination, 20);
 	P->ticketprice = T->ticketprice;
 	P->capacity = T->capacity;
 
@@ -257,24 +256,24 @@ void SwapBuses(struct Bus* L, struct Bus* P) {
 
 int main() {
 	FILE* input;
-	input=fopen("Buses.txt", "r");
+	input=fopen("busses.txt", "r");
 	if(input==NULL) {
-		printf("Buses.txt not found!\n");
+		printf("busses.txt not found!\n");
 		exit(-1);
 	}
 
 	const char EOL = '\n';//end of line reading
 	char character;
-	int lines = 1;
+	int lines = 0;
 	while((character = fgetc(input)) !=EOF){
 		if(character==EOL)
 			lines+=1;
 	}
 	fclose(input);//closing file to reopen it so that it resets the scanner
 
-	FILE *inputB=fopen("Buses.txt", "r");//opening file
+	FILE *inputB=fopen("busses.txt", "r");//opening file
 	if(inputB==NULL){
-		printf("Busses.txt not found!\n");
+		printf("busses.txt not found!\n");
 		exit(-1);
 	}
 	char id[20];
@@ -286,7 +285,7 @@ int main() {
 	int capacity;
 
 	char s[20];
-	memset(s, 0, strlen(s));//resetting the string: solved the problem of printing strange symbols after reading
+	memset(s, 0, sizeof(s));//resetting the string: solved the problem of printing strange symbols after reading
 	int i=0;
 	char temp=fgetc(inputB);
 
@@ -295,7 +294,7 @@ int main() {
 
 	//reading from file
 	for (int j = 0; j < lines; j++) {
-		while (temp != '#') {		//1: Bus ID
+		while (temp != '#' && temp != EOF && i < 19) {		//1: Bus ID
 			if (temp == '\n')
 				temp = fgetc(inputB);
 			s[i] = temp;
@@ -304,10 +303,26 @@ int main() {
 		}
 		temp = fgetc(inputB);
 		i = 0;
-		strlcpy(id, s, 20);
+		strncpy(id, s, 20);
+		//printf("%s\t", id);
 		memset(s, 0, strlen(s));
 
-		while (temp != '#') {		//2: Trip date
+		while (temp != '#' && temp != EOF && i < 19) {		//2: Trip date
+			if (temp == '\n')
+				temp = fgetc(inputB);
+			s[i] = temp;
+			//printf("%c", temp);
+			i++;
+			temp = fgetc(inputB);
+		}
+		temp = fgetc(inputB);
+		i = 0;
+		strncpy(date, s, 20);
+		//printf("\t%s\t", s);
+		//printf("%s\t", date);
+		memset(s, 0, strlen(s));
+
+		while (temp != '#' && temp != EOF && i < 19) {		//3: Trip time
 			if (temp == '\n')
 				temp = fgetc(inputB);
 			s[i] = temp;
@@ -316,10 +331,11 @@ int main() {
 		}
 		temp = fgetc(inputB);
 		i = 0;
-		strlcpy(date, s, 20);
+		strncpy(time, s, 20);
+		//printf("%s\t", time);
 		memset(s, 0, strlen(s));
 
-		while (temp != '#') {		//3: Trip time
+		while (temp != '#' && temp != EOF && i < 19) {	//4: Trip origin
 			if (temp == '\n')
 				temp = fgetc(inputB);
 			s[i] = temp;
@@ -328,10 +344,11 @@ int main() {
 		}
 		temp = fgetc(inputB);
 		i = 0;
-		strlcpy(time, s, 20);
+		strncpy(origin, s, 20);
+		//printf("%s\t", origin);
 		memset(s, 0, strlen(s));
 
-		while (temp != '#') {	//4: Trip origin
+		while (temp != '#' && temp != EOF && i < 19) {	//5: Trip destrination
 			if (temp == '\n')
 				temp = fgetc(inputB);
 			s[i] = temp;
@@ -340,22 +357,11 @@ int main() {
 		}
 		temp = fgetc(inputB);
 		i = 0;
-		strlcpy(origin, s, 20);
+		strncpy(destination, s, 20);
+		//printf("%s\t", destination);
 		memset(s, 0, strlen(s));
 
-		while (temp != '#') {	//5: Trip destrination
-			if (temp == '\n')
-				temp = fgetc(inputB);
-			s[i] = temp;
-			i++;
-			temp = fgetc(inputB);
-		}
-		temp = fgetc(inputB);
-		i = 0;
-		strlcpy(destination, s, 20);
-		memset(s, 0, strlen(s));
-
-		while (temp != '#') {	//5: Trip destrination
+		while (temp != '#' && temp != EOF && i < 19) {	//5: Trip destrination
 			if (temp == '\n')
 				temp = fgetc(inputB);
 			s[i] = temp;
@@ -365,9 +371,10 @@ int main() {
 		temp = fgetc(inputB);
 		i = 0;
 		ticketprice = atoi(s);
+		//printf("%d\t", ticketprice);
 		memset(s, 0, strlen(s));
 
-		while (temp != '\n' && temp != -1) {//4: max students
+		while (temp != '\n' && temp != EOF && i < 19) {//4: max students
 			s[i] = temp;
 			i++;
 			temp = fgetc(inputB);
@@ -376,40 +383,39 @@ int main() {
 			temp = fgetc(inputB);
 		i = 0;
 		capacity = atoi(s);
+		//printf("%d\n", capacity);
 		memset(s, 0, strlen(s));
 
 		InsertBus(id, date, time, origin, destination, ticketprice, capacity, FindLastB(busList), busList);//inserting bus node into linked list
 	}
-
+	PrintBuses(busList);
 	fclose(inputB);
 
 	//reading from passenger file
 	//counting number of lines in file
 	FILE* input2;
-	input2=fopen("Passengers.txt", "r");
+	input2=fopen("passengers.txt", "r");
 	if(input2==NULL) {
-		printf("Passengers.txt not found!\n");
+		printf("passengers.txt not found!\n");
 		exit(-1);
 	}
 
 	char character2;
-	int lines2=1;
+	int lines2=0;
 	while((character2 = fgetc(input)) != EOF){
 		if(character2==EOL)
 			lines2+=1;
 	}
 	fclose(input2);//closing file to reopen it so that it resets the scanner
 
-	FILE *inputs=fopen("Students.txt", "r");//opening file
+	FILE *inputs=fopen("passengers.txt", "r");//opening file
 	if(inputs==NULL){
-		printf("Passengers.txt not found!\n");
+		printf("passengers.txt not found!\n");
 		exit(-1);
 	}
 
 
-	memset(s, 0, strlen(s));//resetting the string array because random symbols are printed otherwise
-
-	memset(s, 0, strlen(s));//resetting the string: solved the problem of printing strange symbols after reading
+	memset(s, 0, sizeof(s));//resetting the string: solved the problem of printing strange symbols after reading
 	i=0;
 	int c=0;
 	char temps=fgetc(inputs);
@@ -420,19 +426,21 @@ int main() {
 	//reading from file
 	for (int j=0; j<lines2; j++) {
 		c=0;
-		while (temps != '#') {//1: student name
+		while (temps != '#' && temps != EOF && i < 19) {//1: student name
 			if (temps == '\n')
 				temps = fgetc(inputs);
 			s[i] = temps;
+			//printf("%c\t", temps);
 			i++;
 			temps = fgetc(inputs);
 		}
 		temps = fgetc(inputs);
 		i = 0;
-		strlcpy(id, s, 20);
+		strncpy(id, s, 20);
+		printf("%s\t", id);
 		memset(s, 0, strlen(s));
 
-		while (temps != '#') {//2: trip date
+		while (temps != '#' && temps != EOF && i < 19) {//2: trip date
 			if (temps == '\n')
 				temps = fgetc(inputs);
 			s[i] = temps;
@@ -441,10 +449,10 @@ int main() {
 		}
 		temps = fgetc(inputs);
 		i = 0;
-		strlcpy(date, s, 20);
+	strncpy(date, s, 20);
 		memset(s, 0, strlen(s));
 
-		while (temps != '#') {//3: trip time
+		while (temps != '#' && temps != EOF && i < 19) {//3: trip time
 			if (temps == '\n')
 				temps = fgetc(inputs);
 			s[i] = temps;
@@ -453,10 +461,10 @@ int main() {
 		}
 		temps = fgetc(inputs);
 		i = 0;
-		strlcpy(time, s, 20);
+		strncpy(time, s, 20);
 		memset(s, 0, strlen(s));
 
-		while (temps != '#') {//4: trip origin
+		while (temps != '#' && temps != EOF && i < 19) {//4: trip origin
 			if (temps == '\n')
 				temps = fgetc(inputs);
 			s[i] = temps;
@@ -465,11 +473,11 @@ int main() {
 		}
 		temps = fgetc(inputs);
 		i = 0;
-		strlcpy(origin, s, 20);
+		strncpy(origin, s, 20);
 		memset(s, 0, strlen(s));
 
 		do {//5: trip destinationn
-			while (temps != '#' && temps!='\n' && temps!=-1) {
+			while (temps != '#' && temps!='\n' && temps != EOF && i < 19) {
 				s[i] = temps;
 				i++;
 				temps = fgetc(inputs);
@@ -477,16 +485,16 @@ int main() {
 			if(temps!='\n')
 				temps = fgetc(inputs);
 		i = 0;
-		strlcpy(destination, s, 20);
+		strncpy(destination, s, 20);
 		memset(s, 0, strlen(s));
 		c++;
-		}while(temps!=-1 && temps!='\n');
+		}while(temps!='\n' && temps != EOF);
 
 			InsertPassenger(id, date, time, origin, destination, FindLastP(passengerList), passengerList);
 			memset(s, 0, strlen(s));
 
 	}
+	PrintPassengers(passengerList);
 	fclose(inputs);
-
 
 }
